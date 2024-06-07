dd6c29170cb6f8ca0de5976281a2fc0ab729016f
행복한엄쌤 <sendmethere@naver.com>
2024-06-07 12:04:21 +0900

    added

--- app/layout.js
+++ app/layout.js
@@ -13,10 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-    <link
-      href="https://fonts.googleapis.com/css2?family=Noto+Color+Emoji"
-      rel="stylesheet"
-    />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Color+Emoji&display=swap" rel="stylesheet">
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
